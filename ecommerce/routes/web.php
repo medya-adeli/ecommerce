235da0823c414a5c4463c910b048c798427eab4f
adolf <midiaadeli12@gmail.com>
2022-12-11 21:45:05 +0330

    push

--- ecommerce/routes/web.php
+++ ecommerce/routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth', 'verified','role:user'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
