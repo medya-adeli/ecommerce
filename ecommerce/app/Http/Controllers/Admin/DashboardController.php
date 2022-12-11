80bff237006b9d13286c370bba8fd3c614822697
adolf <midiaadeli12@gmail.com>
2022-12-11 21:58:29 +0330

    push

--- ecommerce/app/Http/Controllers/Admin/DashboardController.php
+++ ecommerce/app/Http/Controllers/Admin/DashboardController.php
@@ -7,5 +7,7 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    //
+    public function index(){
+        return view('userprofile');
+    }
 }
--- ecommerce/routes/web.php
+++ ecommerce/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -27,5 +28,6 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
+Route::get('/userprofile',[DashboardController::class, 'index']);
+Route::controller([DashboardController::class])->group();
 require __DIR__.'/auth.php';
